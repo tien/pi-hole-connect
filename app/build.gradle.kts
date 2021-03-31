76c8df8357bffdba4d7dc4df18e5f4fdded8dc72
Tien Nguyen Khac <tiennguyenkhac1702@gmail.com>
2021-03-31 19:44:24 +1300

    Bump version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         applicationId = "com.tien.piholeconnect"
         minSdkVersion(21)
         targetSdkVersion(30)
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
