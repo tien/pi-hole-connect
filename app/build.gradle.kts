105c44c231ccee58a5523fdb8037f4903b134439
Tien Nguyen Khac <tiennguyenkhac1702@gmail.com>
2021-04-10 01:30:27 +1200

    Explicitly depend on view model compose integration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,7 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended:$composeVersion")
     implementation("androidx.compose.ui:ui-tooling:$composeVersion")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-alpha04")
     implementation("androidx.activity:activity-compose:1.3.0-alpha06")
     implementation("androidx.navigation:navigation-compose:1.0.0-alpha10")
     implementation("androidx.datastore:datastore:1.0.0-alpha08")
