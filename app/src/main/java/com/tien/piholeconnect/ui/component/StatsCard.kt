e30479bf5636a530bd85f8c4a276e5522e13e8e9
Tien Nguyen Khac <tiennguyenkhac1702@gmail.com>
2021-04-09 14:59:29 +1200

    Auto resize text for stats card

--- app/src/main/java/com/tien/piholeconnect/ui/component/StatsCard.kt
+++ app/src/main/java/com/tien/piholeconnect/ui/component/StatsCard.kt
@@ -2,12 +2,14 @@ package com.tien.piholeconnect.ui.component
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.tien.piholeconnect.ui.theme.contentColorFor
@@ -15,22 +17,35 @@ import com.tien.piholeconnect.ui.theme.success
 
 @Composable
 fun StatsCard(
+    modifier: Modifier = Modifier,
     name: String,
     statistics: String,
-    backGroundColor: Color,
-    modifier: Modifier = Modifier
+    backGroundColor: Color
 ) {
-    Card(backgroundColor = backGroundColor, modifier = modifier) {
+    val defaultSize = MaterialTheme.typography
+    var fontSize by remember { mutableStateOf(defaultSize.h4.fontSize) }
+
+    Card(modifier, backgroundColor = backGroundColor) {
         Column(Modifier.padding(horizontal = 10.dp, vertical = 5.dp)) {
             Text(
                 name,
                 color = contentColorFor(backGroundColor),
-                style = MaterialTheme.typography.subtitle1
+                style = MaterialTheme.typography.subtitle1,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
             )
             Text(
                 statistics,
                 color = contentColorFor(backGroundColor),
-                style = MaterialTheme.typography.h4
+                style = MaterialTheme.typography.h4,
+                fontSize = fontSize,
+                maxLines = 1,
+                softWrap = false,
+                onTextLayout = {
+                    if (it.didOverflowWidth) {
+                        fontSize *= 0.9
+                    }
+                }
             )
         }
     }
@@ -40,8 +55,9 @@ fun StatsCard(
 @Composable
 fun StatsCardPreview() {
     StatsCard(
+        Modifier.widthIn(max = 150.dp),
         name = "Total Queries",
-        statistics = "23,456",
+        statistics = "23,456,756,456",
         backGroundColor = MaterialTheme.colors.success
     )
 }
