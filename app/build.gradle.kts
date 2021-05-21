155b213f272bf425f9eda3f05d937c50c0192f4d
Tien Nguyen Khac <tiennguyenkhac1702@gmail.com>
2021-05-21 15:00:18 +1200

    chore: Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.tien.piholeconnect"
         minSdk = 21
         targetSdk = 30
-        versionCode = 15
-        versionName = "5.0"
+        versionCode = 16
+        versionName = "6.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk {
             debugSymbolLevel = "FULL"
