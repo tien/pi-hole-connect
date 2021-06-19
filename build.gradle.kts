b7751b9b94df78daea8c8426d92c890b45941063
Tien Nguyen Khac <tiennguyenkhac1702@gmail.com>
2021-06-19 15:42:13 +1200

    build: upgrade Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ buildscript {
     extra["composeVersion"] = "1.0.0-beta08"
     extra["ktorVersion"] = "1.6.0"
     extra["protoBufJavaLiteVersion"] = "3.17.3"
-    val hiltVersion by extra("2.36")
+    val hiltVersion by extra("2.37")
     extra["lifecycleVersion"] = "2.3.1"
     extra["cameraxVersion"] = "1.0.0"
     repositories {
@@ -11,7 +11,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.0-beta03")
+        classpath("com.android.tools.build:gradle:7.1.0-alpha02")
         classpath(kotlin("gradle-plugin", version = kotlinVersion))
         classpath("com.google.dagger:hilt-android-gradle-plugin:$hiltVersion")
     }
