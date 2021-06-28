c5366003f6a4155c1b7ed17a9386220e57c5cf04
Tien Nguyen Khac <tiennguyenkhac1702@gmail.com>
2021-06-28 18:47:00 +1200

    perf: free client resources on MainActivity stop

--- app/src/main/java/com/tien/piholeconnect/MainActivity.kt
+++ app/src/main/java/com/tien/piholeconnect/MainActivity.kt
@@ -3,9 +3,12 @@ package com.tien.piholeconnect
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import com.tien.piholeconnect.di.DefaultHttpClient
+import com.tien.piholeconnect.di.TrustAllCertificatesHttpClient
 import com.tien.piholeconnect.service.InAppPurchase
 import com.tien.piholeconnect.ui.App
 import dagger.hilt.android.AndroidEntryPoint
+import io.ktor.client.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -13,9 +16,23 @@ class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var inAppPurchase: InAppPurchase
 
+    @Inject
+    @DefaultHttpClient
+    lateinit var httpClient: HttpClient
+
+    @Inject
+    @TrustAllCertificatesHttpClient
+    lateinit var trustAllCertificatesHttpClient: HttpClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         lifecycle.addObserver(inAppPurchase)
         setContent { App() }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        httpClient.close()
+        trustAllCertificatesHttpClient.close()
     }
 }
