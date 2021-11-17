5c65272f026ed24acace55f46263d8e4df505bd2
Tiến Nguyễn Khắc <tiennguyenkhac1702@gmail.com>
2021-11-17 22:16:33 +1300

    fix: status update is delayed on slow network

--- app/src/main/java/com/tien/piholeconnect/repository/PiHoleRepository.kt
+++ app/src/main/java/com/tien/piholeconnect/repository/PiHoleRepository.kt
@@ -11,6 +11,6 @@ interface PiHoleRepository {
     suspend fun getFilterRules(ruleType: RuleType): PiHoleFilterRules
     suspend fun addFilterRule(rule: String, ruleType: RuleType): ModifyFilterRuleResponse
     suspend fun removeFilterRule(rule: String, ruleType: RuleType): ModifyFilterRuleResponse
-    suspend fun disable(duration: Duration): PiHoleEnableStatus
-    suspend fun enable(): PiHoleEnableStatus
+    suspend fun disable(duration: Duration): PiHoleStatusResponse
+    suspend fun enable(): PiHoleStatusResponse
 }
