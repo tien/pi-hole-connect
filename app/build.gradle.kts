dea69fb98f6c0520c5fe1f20a0deac0e6c0e0b56
Tiến Nguyễn Khắc <tiennguyenkhac1702@gmail.com>
2021-11-17 21:45:23 +1300

    chore: upgrade dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ plugins {
 }
 
 android {
-    compileSdk = 30
+    compileSdk = 31
     buildToolsVersion = "30.0.3"
 
     defaultConfig {
@@ -84,30 +84,30 @@ protobuf {
 dependencies {
     implementation(kotlin("stdlib"))
     implementation(kotlin("reflect"))
-    implementation("androidx.core:core-ktx:1.6.0")
-    implementation("androidx.appcompat:appcompat:1.3.0")
+    implementation("androidx.core:core-ktx:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.compose.ui:ui:$composeVersion")
     implementation("androidx.compose.material:material:$composeVersion")
     implementation("androidx.compose.material:material-icons-extended:$composeVersion")
     implementation("androidx.compose.ui:ui-tooling:$composeVersion")
-    implementation("com.google.accompanist:accompanist-swiperefresh:0.14.0")
+    implementation("com.google.accompanist:accompanist-swiperefresh:0.18.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-alpha07")
-    implementation("androidx.activity:activity-compose:1.3.0-rc02")
-    implementation("androidx.navigation:navigation-compose:2.4.0-alpha04")
-    implementation("androidx.datastore:datastore:1.0.0-rc01")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.4.0")
+    implementation("androidx.activity:activity-compose:1.4.0")
+    implementation("androidx.navigation:navigation-compose:2.4.0-beta02")
+    implementation("androidx.datastore:datastore:1.0.0")
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0-alpha03")
     implementation("androidx.camera:camera-camera2:$cameraxVersion")
     implementation("androidx.camera:camera-lifecycle:$cameraxVersion")
-    implementation("androidx.camera:camera-view:1.0.0-alpha26")
+    implementation("androidx.camera:camera-view:1.0.0-alpha30")
     implementation("io.ktor:ktor-client-core:$ktorVersion")
     implementation("io.ktor:ktor-client-okhttp:$ktorVersion")
     implementation("io.ktor:ktor-client-serialization:$ktorVersion")
     implementation("io.ktor:ktor-client-auth:$ktorVersion")
     implementation("com.google.protobuf:protobuf-javalite:$protoBufJavaLiteVersion")
     implementation("com.google.dagger:hilt-android:$hiltVersion")
-    implementation("com.google.mlkit:barcode-scanning:16.2.0")
+    implementation("com.google.mlkit:barcode-scanning:17.0.0")
     implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
     implementation("com.android.billingclient:billing-ktx:4.0.0")
 
