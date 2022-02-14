7d86ecaa8a6f4b8a431a136e3b0db5ee2ae07e2c
Tien Nguyen Khac <tiennguyenkhac1702@gmail.com>
2022-02-15 10:37:18 +1300

    chore: update dependencies

--- app/src/main/java/com/tien/piholeconnect/ui/component/Scanner.kt
+++ app/src/main/java/com/tien/piholeconnect/ui/component/Scanner.kt
@@ -12,8 +12,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
-import com.google.mlkit.vision.barcode.Barcode
 import com.google.mlkit.vision.barcode.BarcodeScanner
+import com.google.mlkit.vision.barcode.common.Barcode
 import com.google.mlkit.vision.common.InputImage
 
 @OptIn(ExperimentalGetImage::class)
@@ -29,7 +29,7 @@ fun Scanner(
         val viewfinder = PreviewView(context).apply {
             implementationMode = PreviewView.ImplementationMode.COMPATIBLE
         }
-        
+
         val cameraProviderFuture = ProcessCameraProvider.getInstance(context)
 
         cameraProviderFuture.addListener(
@@ -45,21 +45,21 @@ fun Scanner(
                     .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
                     .build().also {
                         it.setAnalyzer(
-                            ContextCompat.getMainExecutor(context),
-                            { imageProxy ->
-                                imageProxy.image?.let { mediaImage ->
-                                    val image = InputImage.fromMediaImage(
-                                        mediaImage,
-                                        imageProxy.imageInfo.rotationDegrees
-                                    )
+                            ContextCompat.getMainExecutor(context)
+                        ) { imageProxy ->
+                            imageProxy.image?.let { mediaImage ->
+                                val image = InputImage.fromMediaImage(
+                                    mediaImage,
+                                    imageProxy.imageInfo.rotationDegrees
+                                )
 
-                                    barcodeScanner.process(image)
-                                        .addOnSuccessListener { barcodes ->
-                                            onBarcodeScanSuccess(barcodes)
-                                            imageProxy.close()
-                                        }
-                                }
-                            })
+                                barcodeScanner.process(image)
+                                    .addOnSuccessListener { barcodes ->
+                                        onBarcodeScanSuccess(barcodes)
+                                        imageProxy.close()
+                                    }
+                            }
+                        }
                     }
 
                 cameraProvider.unbindAll()
