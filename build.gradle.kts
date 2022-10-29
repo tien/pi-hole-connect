2fc93224e4a672e4b2f768b7932df35589727bbd
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2022-10-29 23:14:50 +1300

    build: upgrade Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,8 +9,8 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application") version "7.3.0" apply false
-    id("com.android.library") version "7.3.0" apply false
+    id("com.android.application") version "7.3.1" apply false
+    id("com.android.library") version "7.3.1" apply false
     id("com.google.protobuf") version "0.8.19" apply false
     id("org.jetbrains.kotlin.android") version "1.7.0" apply false
     id("com.google.dagger.hilt.android") version "2.43.2" apply false
