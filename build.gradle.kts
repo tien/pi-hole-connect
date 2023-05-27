508b2d85d4d9fb0007d6f67babc71dc503a605c6
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2023-05-28 02:34:10 +1200

    build: bump Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,8 +9,8 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application") version "8.0.1" apply false
-    id("com.android.library") version "8.0.1" apply false
+    id("com.android.application") version "8.0.2" apply false
+    id("com.android.library") version "8.0.2" apply false
     id("com.google.protobuf") version "0.9.0" apply false
     id("org.jetbrains.kotlin.android") version "1.7.0" apply false
     id("com.google.dagger.hilt.android") version "2.46.1" apply false
