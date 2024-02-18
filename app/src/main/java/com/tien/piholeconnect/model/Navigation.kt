7c91c5e1d0b276b7e0e4546d56fbe3759815fb01
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2024-02-19 00:41:44 +1300

    refactor: use data object

--- app/src/main/java/com/tien/piholeconnect/model/Navigation.kt
+++ app/src/main/java/com/tien/piholeconnect/model/Navigation.kt
@@ -21,15 +21,15 @@ sealed class Screen(
     @StringRes val labelResourceId: Int,
     val options: ScreenOptions
 ) {
-    object Home : Screen("home", R.string.label_home, TabScreenOptions)
-    object Statistics : Screen("statistics", R.string.label_statistics, TabScreenOptions)
-    object Log : Screen("log", R.string.label_log, TabScreenOptions.copy(showTopAppBar = false))
-    object FilterRules : Screen("filterRules", R.string.label_filter_rules, TabScreenOptions)
-    object Preferences : Screen("preferences", R.string.label_preferences, DefaultScreenOptions)
-    object PiHoleConnection :
+    data object Home : Screen("home", R.string.label_home, TabScreenOptions)
+    data object Statistics : Screen("statistics", R.string.label_statistics, TabScreenOptions)
+    data object Log : Screen("log", R.string.label_log, TabScreenOptions.copy(showTopAppBar = false))
+    data object FilterRules : Screen("filterRules", R.string.label_filter_rules, TabScreenOptions)
+    data object Preferences : Screen("preferences", R.string.label_preferences, DefaultScreenOptions)
+    data object PiHoleConnection :
         Screen("piHoleConnection", R.string.label_pi_hole_connection, DefaultScreenOptions)
 
-    object TipJar : Screen("tipJar", R.string.label_tip_jar, DefaultScreenOptions)
+    data object TipJar : Screen("tipJar", R.string.label_tip_jar, DefaultScreenOptions)
 }
 
 fun screenForRoute(route: String) =
