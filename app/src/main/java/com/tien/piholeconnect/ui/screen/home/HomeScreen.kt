8c11faffe62ed7528eb461406311210cad7db5b3
Tiến Nguyễn Khắc <tien.nguyenkhac@icloud.com>
2025-05-31 23:01:48 +1200

    fix: incorrect gravity domain count

--- app/src/main/java/com/tien/piholeconnect/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/tien/piholeconnect/ui/screen/home/HomeScreen.kt
@@ -52,12 +52,12 @@ import com.tien.piholeconnect.ui.theme.infoContainer
 import com.tien.piholeconnect.ui.theme.success
 import com.tien.piholeconnect.ui.theme.successContainer
 import com.tien.piholeconnect.ui.theme.warningContainer
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import java.text.DateFormat
 import java.text.DateFormat.getTimeInstance
 import java.util.Date
 import kotlin.math.max
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
